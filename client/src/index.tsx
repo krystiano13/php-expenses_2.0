0fbdb74d78440308f70362135cb8235120b06fc6
krystiano13 <crystianotv@gmail.com>
2023-11-28 13:39:16 +0100

    configure scss and css minification

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import './styles/base/base.min.css';
 
 const rootDiv:HTMLDivElement = document.querySelector('#root') as HTMLDivElement;
 const root = createRoot(rootDiv);
