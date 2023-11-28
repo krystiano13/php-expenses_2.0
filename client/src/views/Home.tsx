7f590059f2ab477816f506a3ca740ea19d0a5b99
krystiano13 <crystianotv@gmail.com>
2023-11-28 13:58:20 +0100

    import fonts

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import '../styles/Home/Home.min.css';
 
 const Home = () => {
     return (
         <section className="HomeWrapper">
-            <h1>Expense CSV Reader</h1>
-            <form>
-                <input type="file" accept=".csv" placeholder="Upload CSV file"/>
-                <button type="submit">Submit</button>
+            <h1 className="HomeWrapper__title">Expense CSV Reader</h1>
+            <form className="HomeWrapper__form">
+                <input className="HomeWrapper__form__input" type="file" accept=".csv" placeholder="Upload CSV file"/>
+                <button className="HomeWrapper__button" type="submit">Submit</button>
             </form>
         </section>
     )
