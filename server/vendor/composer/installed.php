7b982636739e9c5492fc5e6e97286099e672115f
krystiano13 <crystianotv@gmail.com>
2023-11-28 13:19:46 +0100

    configure server

---
+++ server/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'krystian/server',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2f512e961fcdac3621a52cdb5c6ccd80d1824d15',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'krystian/server' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2f512e961fcdac3621a52cdb5c6ccd80d1824d15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
