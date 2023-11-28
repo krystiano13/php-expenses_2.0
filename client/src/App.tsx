2f512e961fcdac3621a52cdb5c6ccd80d1824d15
krystiano13 <crystianotv@gmail.com>
2023-11-28 13:18:14 +0100

    configure client side

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>PHP expenses 2.0</h1>
+        </div>
+    )
+}
+
+export { App };
---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const rootDiv:HTMLDivElement = document.querySelector('#root') as HTMLDivElement;
+const root = createRoot(rootDiv);
+
+root.render(
+    <>
+        <App />
+    </>
+)
