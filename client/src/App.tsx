a2b71fcf9721aa08af3f6c3f00ff4641e50a0a82
krystiano13 <crystianotv@gmail.com>
2023-11-28 13:50:44 +0100

    configure react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from "./views/Home";
 
 const App = () => {
     return (
-        <div>
-            <h1>PHP expenses 2.0</h1>
-        </div>
+        <main>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </main>
     )
 }
 
