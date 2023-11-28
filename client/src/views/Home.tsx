6f9daa8722f7729a5c87203b3785507c43f93a5a
krystiano13 <crystianotv@gmail.com>
2023-11-28 13:53:56 +0100

    create structure of Home component

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <section className="HomeWrapper">
+            <h1>Expense CSV Reader</h1>
+            <form>
+                <input type="file" accept=".csv" placeholder="Upload CSV file"/>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
     )
 }
 
