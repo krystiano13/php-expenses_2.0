fef8c2710ef238344a48df9aae5c8c3041ce7f99
krystiano13 <crystianotv@gmail.com>
2023-11-29 12:37:26 +0100

    create structure of Instruction view

--- client/src/views/Instruction.tsx
+++ client/src/views/Instruction.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Instruction = () => {
     return (
-        <section></section>
+        <section className="Instruction">
+            <h1 className="Instruction__title">Instruction</h1>
+            <p className="Instruction__content">Columns of the imported CSV table should be named: Date,Check #,Description and Amount</p>
+            <button className="Instruction__button">Go Back</button>
+        </section>
     )
 }
 
