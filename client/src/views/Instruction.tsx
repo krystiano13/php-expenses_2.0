eede3d37af4daf7a193dfc7d2dcf356757804fb4
krystiano13 <crystianotv@gmail.com>
2023-11-29 12:57:38 +0100

    create button for redirecting to home

--- client/src/views/Instruction.tsx
+++ client/src/views/Instruction.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useNavigate } from "react-router";
 import '../styles/Instruction/Instruction.min.css';
 
 const Instruction = () => {
+    const navigate = useNavigate();
     return (
         <section className="Instruction">
             <h1 className="Instruction__title">Instruction</h1>
             <p className="Instruction__content">Columns of the imported CSV table should be named: Date,Check #,Description and Amount</p>
-            <button className="Instruction__button">Go Back</button>
+            <button
+                onClick={() => navigate('/')}
+                className="Instruction__button">
+                Go Back
+            </button>
         </section>
     )
 }
