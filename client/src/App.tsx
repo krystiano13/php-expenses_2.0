60e5c4e5b88a903c3714123c4c9a398be0d07c4d
krystiano13 <crystianotv@gmail.com>
2023-11-29 12:32:49 +0100

    create route for instruction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from "./views/Home";
+import { Instruction } from "./views/Instruction";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Home />} />
+                    <Route path='/instruction' element={<Instruction />} />
                 </Routes>
             </BrowserRouter>
         </main>
