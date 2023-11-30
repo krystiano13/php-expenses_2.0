6dba496353bb2a4834f67f44126cb730596f4338
krystiano13 <crystianotv@gmail.com>
2023-11-30 13:02:05 +0100

    create route for table view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from "./views/Home";
 import { Instruction } from "./views/Instruction";
+import { Table } from "./views/Table";
 
 const App = () => {
     const [data,setData] = useState<string[]>([]);
@@ -11,6 +12,7 @@ const App = () => {
                 <Routes>
                     <Route path='/' element={<Home setData={(arr:string[]) => setData(arr)} />} />
                     <Route path='/instruction' element={<Instruction />} />
+                    <Route path='/table' element={<Table />} />
                 </Routes>
             </BrowserRouter>
         </main>
---
+++ client/src/views/Table.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <section className="Table"></section>
+    )
+}
+
+export { Table };
