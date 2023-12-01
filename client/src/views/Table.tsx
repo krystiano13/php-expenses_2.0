2bf63ca32b05da9d695028a0e1de528cba3ec81a
krystiano13 <crystianotv@gmail.com>
2023-12-01 10:47:40 +0100

    add tbdody and thead tags

--- client/src/views/Table.tsx
+++ client/src/views/Table.tsx
@@ -1,34 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '../styles/Table/Table.min.css';
 
-import type { income } from "../App";
+import type {income} from "../App";
 
 interface Props {
     data: string[][],
     income: income
 }
 
-const Table:React.FC<Props> = ({ data, income }) => {
+const Table: React.FC<Props> = ({data, income}) => {
     return (
         <section className="Table">
             <table>
+                <thead>
                 <tr>
                     <th>Date</th>
                     <th>Check #</th>
                     <th>Description</th>
                     <th>Amount</th>
                 </tr>
+                </thead>
+                <tbody>
                 {
                     data.map(item => (
                         <tr>
                             {
                                 item.map(element => (
-                                    <td>{ element }</td>
+                                    <td>{element}</td>
                                 ))
                             }
                         </tr>
                     ))
                 }
+                </tbody>
             </table>
             <h2>Total Income: {income?.income.toFixed(2)}</h2>
             <h2>Total Expenses: {income?.expenses.toFixed(2)}</h2>
@@ -37,4 +41,4 @@ const Table:React.FC<Props> = ({ data, income }) => {
     )
 }
 
-export { Table };
+export {Table};
